b53aa200ea1a3447d2ae2f0dd5c570292a67cb08
murilofsouzaa <onemurilo@gmail.com>
2026-04-29 22:01:15 -0300

    feat: adding setInterval to change title every 2 seconds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import {Routes, Route, useLocation} from 'react-router-dom'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 import Homepage  from './pages/Homepage/Homepage.tsx';
 import ProductDetail from './pages/DetailsPage/DetailsPage.tsx'
@@ -10,7 +10,25 @@ if ('scrollRestoration' in history) {
 
 function App() {
 
+  const titles = ["Compre AGORA sua oversized!", "Tá barato pô, compra ai"]
   const location = useLocation();
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+    
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            setCurrentIndex((prev) => (prev + 1) % titles.length)
+        }, 1000);
+    
+        return () =>{
+            clearInterval(interval);
+        }
+    }, []);
+
+    useEffect(() => {
+      document.title=titles[currentIndex]
+    }, [currentIndex]);
 
   useEffect(() => {
     window.scrollTo(0,0)
