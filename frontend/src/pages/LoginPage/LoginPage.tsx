ec924383476574da779c60c4006ae609f2fd9717
murilofsouzaa <onemurilo@gmail.com>
2026-05-11 10:01:24 -0300

    feat: implementing labels and inputs for login page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,38 @@
-import {Footer} from '../../layout/Footer/Footer'
+import {useState} from 'react'
+import edenLogo from '../../../public/logo/logo.png'
+import {Eye} from 'lucide-react'
+import {EyeClosed} from 'lucide-react'
+
 
 const LoginPage = () => {
+    const [active, setActive] = useState(false)
+
     return ( 
-        <>
-            <div className="bg-linear-to-b/decreasing from-white from-50% via-white via-60% to-black h-[100vh]">
-                <div className=""> 
-                    <h2>Login</h2>
-                </div>
+        <div className="flex flex-col justify-center items-center h-screen"> 
+            <img src={edenLogo} alt="Eden" className="h-22 w-auto mb-10" />
+            <div className="flex flex-col justify-center items-center">   
+                <h2 className=" text-2xl font-bold">EDEN LOGIN</h2>
+                <p className="text-sm text-black/70 mb-5 mt-1">Compre seus estilos e treine conosco.</p>
+
+                <form className="flex flex-col gap-5">
+                    <input type="text" name="email" id="email" placeholder="Email*"
+                    className="outline-0 border border-black/30 py-3 px-4"/>
+                    <div>
+                        <div className="flex border border-black/30 py-3 px-6">
+                            <input type="password" id="password" className="outline-0" placeholder="Senha*"/>
+                            <button type="button" className="flex justify-center items-center hover:cursor-pointer"
+                            onClick={() => setActive((prev) => !prev)}>
+                                {active == true ? (
+                                    <EyeClosed className="text-black/40 h-5 w-auto hover:text-black/60"></EyeClosed>
+                                ) : (
+                                    <Eye className="text-black/40 h-5 w-auto hover:text-black/60"></Eye>
+                                )}
+                            </button>
+                        </div> 
+                    </div>
+                </form>
             </div>
-            <Footer></Footer>
-        </>
+        </div>
      );
 }
  
