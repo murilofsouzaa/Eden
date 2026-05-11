5201a5e024cd762fc83c4dd3c6183fa47aea24b3
murilofsouzaa <onemurilo@gmail.com>
2026-05-11 11:05:06 -0300

    feat(login): implementing login page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -9,26 +9,34 @@ const LoginPage = () => {
 
     return ( 
         <div className="flex flex-col justify-center items-center h-screen"> 
-            <Link to="/"><img src={edenLogo} alt="Eden" className="h-22 w-auto mb-10" /></Link>
+            <Link to="/"><img src={edenLogo} alt="Eden" className="h-22 w-auto" /></Link>
             <div className="flex flex-col justify-center items-center">   
-                <h2 className=" text-2xl font-bold">EDEN LOGIN</h2>
-                <p className="text-sm text-black/70 mb-5 mt-1">Compre seus estilos e treine conosco.</p>
+                <h2 className=" text-2xl font-bold m-5">EDEN LOGIN</h2>
+                <p className="text-sm font-sans text-black/70 mb-5 w-[80%] text-center">Compre seus estilos, sinta-se confiante e treine conosco.</p>
 
                 <form className="flex flex-col gap-5">
                     <input type="text" name="email" id="email" placeholder="Email*"
-                    className="outline-0 border border-black/30 py-3 px-4"/>
+                    className="outline-0 border border-black/10 py-3 px-4"/>
                     <div>
-                        <div className="flex border border-black/30 py-3 px-6">
-                            <input type="password" id="password" className="outline-0" placeholder="Senha*"/>
-                            <button type="button" className="flex justify-center items-center hover:cursor-pointer"
-                            onClick={() => setActive((prev) => !prev)}>
-                                {active == true ? (
-                                    <EyeClosed className="text-black/40 h-5 w-auto hover:text-black/60"></EyeClosed>
-                                ) : (
-                                    <Eye className="text-black/40 h-5 w-auto hover:text-black/60"></Eye>
-                                )}
-                            </button>
+                        <div className="flex border border-black/10">
+                            <input type="password" id="password" className="outline-0 py-3 px-6" placeholder="Senha*"/>
+                            <div onClick={() => setActive((prev) => !prev)}
+                            className="flex justify-center items-center py-3 px-6 hover:cursor-pointer hover:bg-gray-100">
+                                <button type="button" className="hover:cursor-pointer">
+                                    {active == true ? (
+                                        <EyeClosed className="text-black/40 h-5 w-auto hover:text-black/60"></EyeClosed>
+                                    ) : (
+                                        <Eye className="text-black/40 h-5 w-auto hover:text-black/60"></Eye>
+                                    )}
+                                </button>
+                            </div>
                         </div> 
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <button type="button" className="text-sm underline font-bold hover:cursor-pointer">Esqueceu a senha?</button>
+                        <button type="button" className="m-5 text-md font-bold text-white bg-black rounded-4xl py-3 w-full
+                        cursor-pointer active:scale-[0.97] transition-all">LOGIN</button>
+                        <p className="text-sm text-black/80">Não possui conta? <span className="font-bold text-sm text-black hover:cursor-pointer hover:underline">Criar Conta</span></p>
                     </div>
                 </form>
             </div>
