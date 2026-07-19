2557cf094f065e5f31e66b0e0355c362aa1067bf
Murilo <onemurilo@gmail.com>
2026-07-18 21:45:00 -0300

    feat: changing sizu buttons margin

--- frontend/src/pages/DetailsPage/Info/components/SizeButtons/SizeButtons.tsx
+++ frontend/src/pages/DetailsPage/Info/components/SizeButtons/SizeButtons.tsx
@@ -10,9 +10,9 @@ const SizeButtons = ({selectedSize="P", handleSizeClick}:SizeButtonsProps) => {
     const sizes = ["P", "M", "G", "GG", "XGG", "XGGG"]
 
     return ( 
-        <div className="font-sans border-t border-t-[#a5a5a59e] py-5">
-            <label className=" font-semibold text-md">Tamanho: {selectedSize}</label>
-            <div className="flex gap-4 p-2 mt-2 w-full">
+        <div className="font-sans border-t border-t-[#a5a5a59e] py-5 ">
+            <label className="font-semibold text-md">Tamanho: {selectedSize}</label>
+            <div className="flex gap-2 p-2 mt-2 justify-center items-center">
                 {sizes.map((size) => (
                     <button key={size} 
                         className={` flex justify-center items-center font-sans size-button  ${size === selectedSize ? 'active' : ''}`}
