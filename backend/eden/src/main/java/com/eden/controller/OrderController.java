dc1460686e8334a19100e685a9755bdc342c948f
Murilo <onemurilo@gmail.com>
2026-07-19 09:57:32 -0300

    feat: adding cors imports

--- backend/eden/src/main/java/com/eden/controller/OrderController.java
+++ backend/eden/src/main/java/com/eden/controller/OrderController.java
@@ -7,7 +7,7 @@ import com.eden.service.order.OrderService;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
--- backend/eden/src/main/java/com/eden/controller/ShoppingCartController.java
+++ backend/eden/src/main/java/com/eden/controller/ShoppingCartController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.eden.dto.shopping_cart.ShoppingCartResponse;
 import com.eden.dto.shopping_cart.cart_item.AddItemCartRequest;
 import com.eden.dto.shopping_cart.cart_item.ItemCartResponse;
